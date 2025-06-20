fadc714d0ab949b09f4eddcc3a0449de6432b1f4
Abdullahi Osman <167461783+abdull6hi@users.noreply.github.com>
2025-06-20 10:31:09 +0300

    Update selection_sort.py

--- selection_sort/selection_sort.py
+++ selection_sort/selection_sort.py
@@ -1,15 +1,9 @@
 def selection_sort(arr):
-    arr = list(unsorted_list)  
     n = len(arr)
-
-    for i in range(n):
+    for i in range(n - 1):
         min_index = i
-
         for j in range(i + 1, n):
             if arr[j] < arr[min_index]:
                 min_index = j
-
-        if min_index != i:
-            arr[i], arr[min_index] = arr[min_index], arr[i]
-
+        arr[i], arr[min_index] = arr[min_index], arr[i]
     return arr
